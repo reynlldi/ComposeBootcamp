c8a100e8de1b4adf76080c4e848596322834ec00
Reynlldi <reynalddi93@gmail.com>
2024-06-12 22:56:06 +0700

    fundamental 2

--- app/src/main/java/com/example/testing/HomeScreen.kt
+++ app/src/main/java/com/example/testing/HomeScreen.kt
@@ -10,15 +10,19 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.LinearProgressIndicator
+import androidx.compose.material.Slider
 import androidx.compose.material.TextButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
@@ -26,17 +30,32 @@ import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.RangeSlider
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -49,11 +68,19 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
+    // snackBar
+    val scope = rememberCoroutineScope()
+    val snackBarHostState = remember { SnackbarHostState() }
+
     Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackBarHostState)
+        },
         topBar = {
             CenterAlignedTopAppBar(
                 title = {
@@ -87,7 +114,21 @@ fun HomeScreen(modifier: Modifier = Modifier) {
             )
         },
         floatingActionButton = {
-            FloatingActionButton(onClick = { /*TODO*/ }) {
+            FloatingActionButton(
+                onClick = {
+                    scope.launch {
+                        val result = snackBarHostState.showSnackbar(
+                            message = "Hallo semuanya!",
+                            actionLabel = "Undo",
+                            duration = SnackbarDuration.Short
+                        )
+                        when(result){
+                            SnackbarResult.ActionPerformed -> {}
+                            SnackbarResult.Dismissed -> {}
+                        }
+                    }
+                }
+            ) {
                 Icon(imageVector = Icons.Default.Add, contentDescription = "icon add")
             }
         },
@@ -106,15 +147,32 @@ fun HomeScreen(modifier: Modifier = Modifier) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreenContent(modifier: Modifier = Modifier) {
+    // expand text
     var expanded by remember { mutableStateOf(false) }
+
+    // dialog alert
     var showDialog by remember { mutableStateOf(false) }
+
+    // modal bottom sheet
+    val sheetState = rememberModalBottomSheetState()
+    var showBottomSheet by remember { mutableStateOf(false) }
+
+    // slider
+    var slider by remember { mutableFloatStateOf(0f) }
+    var sliderRange by remember { mutableStateOf(0f..100f) }
+
+    // switch button
+    var switch by remember { mutableStateOf(false) }
 
     Column(
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier.fillMaxSize()
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
     ) {
         Row(
             horizontalArrangement = Arrangement.Center,
@@ -157,7 +215,12 @@ fun HomeScreenContent(modifier: Modifier = Modifier) {
                 }
             }
         }
-        Button(onClick = { showDialog = true }) {
+
+        // alert dialog
+        Button(
+            onClick = { showDialog = true },
+            shape = CircleShape
+        ) {
             Text(text = "Alert Dialog")
         }
         if (showDialog) {
@@ -167,6 +230,84 @@ fun HomeScreenContent(modifier: Modifier = Modifier) {
                 onDismiss = { showDialog = false }
             )
         }
+
+        // button bottom sheet
+        FilledTonalButton(onClick = { showBottomSheet = true }) {
+            Text(text = "Show Bottom Modal Sheet")
+        }
+        if (showBottomSheet) {
+            ModalBottomSheet(
+                sheetState = sheetState,
+                onDismissRequest = { showBottomSheet = false }
+            ) {
+                Text(
+                    text = "ini Bottom modal sheet",
+                    modifier = Modifier.padding(bottom = 80.dp)
+                )
+            }
+        }
+
+        // card
+        Card(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .width(200.dp)
+                .height(100.dp)
+        ) {
+            Text(
+                text = "Ini card default",
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+
+        OutlinedCard(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .width(200.dp)
+                .height(100.dp)
+        ) {
+            Text(
+                text = "Ini card outlined",
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+
+        // slider biasa
+        Slider(
+            value = slider,
+            onValueChange = { slider = it },
+            steps = 20, // kasih gapnya
+            valueRange = 0f..100f
+        )
+
+        // slider range
+        RangeSlider(
+            value = sliderRange,
+            onValueChange = { sliderRange = it },
+            steps = 20, // kasih gapnya
+            valueRange = 0f..100f
+        )
+
+        // loading circle
+        CircularProgressIndicator(
+            color = MaterialTheme.colorScheme.primary
+        )
+
+        // loading horizontal
+        LinearProgressIndicator()
+
+        // switch button
+        Switch(
+            checked = switch,
+            onCheckedChange = {
+                switch = it
+            },
+            thumbContent = {
+                if (switch) {
+                    Icon(imageVector = Icons.Default.Check, contentDescription = "icon check")
+                }
+            }
+        )
     }
 }
 
