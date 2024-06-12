1b1f13a9cb94f9887f9fbbea7dbb6c9493f18c30
Reynlldi <reynalddi93@gmail.com>
2024-06-12 21:35:07 +0700

    fundamental 2

--- app/src/main/java/com/example/testing/HomeScreen.kt
+++ app/src/main/java/com/example/testing/HomeScreen.kt
@@ -8,14 +8,31 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.TextButton
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -33,49 +50,152 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    var expanded by remember { mutableStateOf(false) }
-
-    Row(
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = modifier.padding(8.dp)
-    ) {
-        Box {
-            Image(
-                painter = painterResource(id = R.drawable.albert),
-                contentDescription = "foto albert einstein",
-                modifier = Modifier
-                    .border(2.dp, Color.Blue, CircleShape)
-                    .size(60.dp)
-                    .clip(CircleShape)
-                    .clickable {
-                        expanded = !expanded
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(text = "Home Screen")
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Blue
+                ),
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Default.Menu,
+                            contentDescription = "icon menu"
+                        )
                     }
-                    .alpha(0.5f),
-                contentScale = ContentScale.Crop
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Default.Notifications,
+                            contentDescription = "notification"
+                        )
+                    }
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Default.FavoriteBorder,
+                            contentDescription = "favorite"
+                        )
+                    }
+                }
             )
-            Icon(
-                imageVector = Icons.Default.CheckCircle,
-                contentDescription = "icon check",
-                modifier = Modifier.align(Alignment.BottomEnd),
-                tint = Color.Blue
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "icon add")
+            }
+        },
+        bottomBar = {
+            androidx.compose.material3.BottomAppBar(
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Default.Edit, contentDescription = "icon edit")
+                    }
+                }
             )
-        }
-        Spacer(modifier = Modifier.width(8.dp))
-        Column {
-            Text(text = "Albert Einstein")
-            Text(
-                text = "2 Years Ago",
-                fontWeight = FontWeight.ExtraLight,
-                fontSize = 8.sp
-            )
-            if (expanded){
-                Text(text = "Pernah tinggal di batam")
+        },
+        modifier = modifier
+    ) { paddingValues ->
+        HomeScreenContent(modifier = Modifier.padding(paddingValues))
+    }
+}
+
+@Composable
+fun HomeScreenContent(modifier: Modifier = Modifier) {
+    var expanded by remember { mutableStateOf(false) }
+    var showDialog by remember { mutableStateOf(false) }
+
+    Column(
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+        ) {
+            Box {
+                Image(
+                    painter = painterResource(id = R.drawable.albert),
+                    contentDescription = "foto albert einstein",
+                    modifier = Modifier
+                        .border(2.dp, Color.Blue, CircleShape)
+                        .size(60.dp)
+                        .clip(CircleShape)
+                        .clickable {
+                            expanded = !expanded
+                        }
+                        .alpha(0.9f),
+                    contentScale = ContentScale.Crop
+                )
+                Icon(
+                    imageVector = Icons.Default.CheckCircle,
+                    contentDescription = "icon check",
+                    modifier = Modifier.align(Alignment.BottomEnd),
+                    tint = Color.Blue
+                )
+            }
+            Spacer(modifier = Modifier.width(8.dp))
+            Column {
+                Text(text = "Albert Einstein")
+                Text(
+                    text = "2 Years Ago",
+                    fontWeight = FontWeight.ExtraLight,
+                    fontSize = 8.sp
+                )
+                if (expanded) {
+                    Text(text = "Pernah tinggal di batam")
+                }
             }
         }
+        Button(onClick = { showDialog = true }) {
+            Text(text = "Alert Dialog")
+        }
+        if (showDialog) {
+            ExampleDialog(
+                onCancel = { showDialog = false },
+                onConfirm = { showDialog = false },
+                onDismiss = { showDialog = false }
+            )
+        }
     }
+}
+
+@Composable
+fun ExampleDialog(
+    modifier: Modifier = Modifier,
+    onDismiss: () -> Unit,
+    onConfirm: () -> Unit,
+    onCancel: () -> Unit
+) {
+    AlertDialog(
+        icon = {
+            Icon(imageVector = Icons.Default.Delete, contentDescription = "icon delete")
+        },
+        title = { Text(text = "HAPUS") },
+        text = { Text(text = "Apakah yakin mau hapus data?") },
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            TextButton(onClick = onConfirm) {
+                Text(text = "Hapus")
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = onCancel) {
+                Text(text = "Batal")
+            }
+        },
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
